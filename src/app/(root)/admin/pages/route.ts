7669d9761a6b29caa0154a3ccda9490f85e90677
Ran429 <145637014+Ran429@users.noreply.github.com>
2025-09-11 14:11:03 +0900

    Delete route.ts

--- src/app/(root)/admin/pages/route.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/server/prisma/prisma.client";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const pages = await prisma.page.findMany({
-    orderBy: { updatedAt: "desc" },
-  });
-  return NextResponse.json(pages);
-}
